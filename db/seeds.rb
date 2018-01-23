4938608409bb152fa60479e38fdf3eae6e374003
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-01-23 17:23:20 +0000

    leaderboard formatting

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+cyclist = {username: "roadie", first_name: "Miss", last_name: "Roadie", age: "30", email: "missroadie@ride.com", password: "password", password_confirmation: "password"}
+Cyclist.create(cyclist)
